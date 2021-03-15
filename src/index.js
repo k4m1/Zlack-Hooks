e8a3285233de8224afb1317277f91f6df660e160
k4m1 <zachary.lyons.johnson@gmail.com>
2021-03-15 15:17:22 -0400

    added boilerplate and routes

--- src/index.js
+++ src/index.js
@@ -4,24 +4,22 @@ import './index.css';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-
+import Register from './components/Auth/Register';
+import Login from './components/Auth/Login';
 
 const Root = () => (
   <Router>
     <Switch>
       <Route path="/" component={App} />
+      <Route path="/login" component={Login} />
+      <Route path="/register" component={Register} />
     </Switch>
   </Router>
 )
 
 
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Root />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+ReactDOM.render(<Root />, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
